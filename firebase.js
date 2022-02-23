433b38fcb9eb1b766c8701105bcdf32bfd821a1b
Ozan Gürer <ozangurer12@gmail.com>
2022-02-23 13:39:44 +0300

    firebase dosyası düzenlendi

--- firebase.js
+++ firebase.js
@@ -1,16 +1,6 @@
 import * as firebase from 'firebase';
 import "firebase/storage";
-
-export const firebaseConfig = {
-    apiKey: "AIzaSyDIn1_N_W_rCcNkYa-klAlUcQhYWABBC1A",
-    authDomain: "adroit-nuance-315720.firebaseapp.com",
-    databaseURL: "https://adroit-nuance-315720-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "adroit-nuance-315720",
-    storageBucket: "adroit-nuance-315720.appspot.com",
-    messagingSenderId: "631511442516",
-    appId: "1:631511442516:web:7c5ed645044067692f39c1",
-    measurementId: "G-24792FE8K4"
-};
+import { firebaseConfig } from './env.js'
 
 let app;
 
